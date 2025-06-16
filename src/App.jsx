2247579b0f1f1bb1ee3c6c0a5c3483ddedf5b965
İbrahim Koçal <ibrahimkocal@gmail.com>
2025-06-17 00:33:01 +0300

    şimdilik hazır listedeki film bilgileri eklenip ekrana verildi.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,73 @@
 import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
+const movie_list = [
+  {
+    Id: "769",
+    Title: "GoodFellas",
+    Year: "1990",
+    Poster:
+      "https://image.tmdb.org/t/p/original/aKuFiU82s5ISJpGZp7YkIr3kCUd.jpg",
+  },
+  {
+    Id: "120",
+    Title: "The Lord of the Rings",
+    Year: "2001",
+    Poster:
+      "https://image.tmdb.org/t/p/original/6oom5QYQ2yQTMJIbnvbkBL9cHo6.jpg",
+  },
+  {
+    Id: "27205",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://image.tmdb.org/t/p/original/ljsZTbVsrQSqZgWeep2B1QiDKuh.jpg",
+  },
+  ,
+  {
+    Id: "105",
+    Title: "Back to the Future",
+    Year: "1985",
+    Poster:
+      "https://image.tmdb.org/t/p/original/fNOH9f1aA7XRTzl1sAOx9iF553Q.jpg",
+  },
+];
+
+const selected_movie_list = [
+  {
+    Id: "769",
+    Title: "GoodFellas",
+    Year: "1990",
+    Poster:
+      "https://image.tmdb.org/t/p/original/aKuFiU82s5ISJpGZp7YkIr3kCUd.jpg",
+    duration: 120,
+    rating: 8.4,
+  },
+  {
+    Id: "120",
+    Title: "The Lord of the Rings",
+    Year: "2001",
+    Poster:
+      "https://image.tmdb.org/t/p/original/6oom5QYQ2yQTMJIbnvbkBL9cHo6.jpg",
+    duration: 125,
+    rating: 8.8,
+  },
+  {
+    Id: "105",
+    Title: "Back to the Future",
+    Year: "1985",
+    Poster:
+      "https://image.tmdb.org/t/p/original/fNOH9f1aA7XRTzl1sAOx9iF553Q.jpg",
+    duration: 125,
+    rating: 8.8,
+  },
+];
+
+const getAverage = (array) =>
+  array.reduce((sum, value) => sum + value, 2) / array.length;
+
+export default function App() {
+  const [movies, setMovies] = useState(movie_list);
+  const [selectedMovies, setSelectedMovies] = useState(selected_movie_list);
 
   return (
     <>
@@ -10,6 +76,22 @@ function App() {
         <Search />
         <NavSearchResults />
       </Nav>
+      <Main>
+        <div className="row mt-">
+          <div className="col-md-9">
+            <ListContainer>
+              <MovieList movies={movies} />
+            </ListContainer>
+          </div>
+          <div className="col-md-3">
+            <ListContainer>
+              <>
+                <MyListSummary selectedMovies={selectedMovies} />
+              </>
+            </ListContainer>
+          </div>
+        </div>
+      </Main>
     </>
   );
 }
@@ -49,4 +131,82 @@ function NavSearchResults() {
   );
 }
 
-export default App;
+function Main({ children }) {
+  return <main className="container">{children}</main>;
+}
+
+function ListContainer({ children }) {
+  const [isOpen, setIsOpen] = useState(true);
+  return (
+    <div className="movie-list">
+      <button
+        className="btn btn-sm btn-outline-dark mt-3"
+        onClick={() => setIsOpen((val) => !val)}
+      >
+        {isOpen ? (
+          <i className="bi bi-chevron-up"></i>
+        ) : (
+          <i className="bi bi-chevron-down"></i>
+        )}
+      </button>
+      {isOpen && children}
+    </div>
+  );
+}
+
+function MovieList({ movies }) {
+  return (
+    <div className="row row-cols-1 row-cols-md-3 row-cols-xl-4 g-4 mt-1">
+      {movies.map((movie) => (
+        <Movie movie={movie} key={movie.Id} />
+      ))}
+    </div>
+  );
+}
+
+function Movie({ movie }) {
+  return (
+    <div className="col mb-2">
+      <div className="card">
+        <img
+          src={
+            movie.Poster
+              ? `https://media.themoviedb.org/t/p/w440_and_h660_face` +
+                movie.Poster
+              : "/img/no-image.jpg"
+          }
+          alt={movie.Title}
+        />
+        <div className="card-body">
+          <h6 className="card-title">{movie.Title}</h6>
+          <div>
+            <i className="bi bi-calendar2-date me-1"></i>
+            <span>{movie.Year}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function MyListSummary({ selectedMovies }) {
+  const avgRating = getAverage(selectedMovies.map((m) => m.rating));
+  const avgDuration = getAverage(selectedMovies.map((m) => m.duration));
+  return (
+    <div className="card mb-2">
+      <div className="card-body">
+        <h5>Listeye [{selectedMovies.length}] film eklendi.</h5>
+        <div className="d-flex justify-content-between">
+          <p>
+            <i className="bi bi-star-fill text-warning me-1"></i>
+            <span>{avgRating.toFixed(2)}</span>
+          </p>
+          <p>
+            <i className="bi bi-hourglass-split text-warning me-1"></i>
+            <span>{avgDuration} dk</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
