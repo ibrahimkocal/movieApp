6c115c882c0b5638b339c3bca483ff80242893c1
İbrahim Koçal <ibrahimkocal@gmail.com>
2025-06-16 23:46:36 +0300

    navbar düzenlemesi

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,47 @@ function App() {
 
   return (
     <>
-      <Nav />
+      <Nav>
+        <Logo />
+        <Search />
+        <NavSearchResults />
+      </Nav>
     </>
   );
 }
 
-function Nav() {
+function Nav({ children }) {
   return (
-    <nav className="bg-primary text-white p-2">
+    <nav className="bg-dark text-white p-2">
       <div className="container">
-        <div className="row align-items-center">5 kayıt bulundu</div>
+        <div className="row align-items-center">{children}</div>
       </div>
     </nav>
+  );
+}
+
+function Logo() {
+  return (
+    <div className="col-4">
+      <i className="bi bi-camera-reels me-2"></i>
+      Movie App
+    </div>
+  );
+}
+
+function Search() {
+  return (
+    <div className="col-4">
+      <input type="text" className="form-control" placeholder="Film ara..." />
+    </div>
+  );
+}
+
+function NavSearchResults() {
+  return (
+    <div className="col-4 text-end">
+      <strong>5</strong> kayıt bulundu.
+    </div>
   );
 }
 
